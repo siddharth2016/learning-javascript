230b88c99d7b3afecd23d77b726c59c04e5d51ca
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-14 18:42:06 +0530

    semicolon pitfalls exceptions

---
+++ chapterTwo/semicolonPitfalls.js
@@ -0,0 +1,23 @@
+console.log("Example 1");
+
+let a
+a
+=
+3
+console.log(a)
+
+
+console.log("Example 2");
+
+let b = a + a   // pitfall, JS will not put a semicolon after this line
+(1+2).toString()
+console.log(b)
+
+console.log("Example 3");
+
+a++ // exception, JS will put a semicolon after this line
+b++ 
+
+// JS will put a semicolon after return, yield, throw, break and continue
+
+// JS will put a semicolon before => if not used with function arguments
