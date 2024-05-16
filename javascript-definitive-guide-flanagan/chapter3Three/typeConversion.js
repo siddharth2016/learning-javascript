693b94d29f1a114d711764064c5c62f14a244d24
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-16 23:05:31 +0530

    type conversions

---
+++ javascript-definitive-guide-flanagan/chapter3Three/typeConversion.js
@@ -0,0 +1,13 @@
+console.log(7 + "string");
+console.log("5" * "6");
+console.log(1 - "x");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+
+console.log(123.234.toFixed(3));
+console.log(123.234.toExponential(3));
+console.log(123.234.toPrecision(3));
+
+console.log(parseFloat("1234"));
+console.log(parseInt("1234.986398"));
