6a0c97aa95d2e32fdd00d4d2039a1246a33e0353
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-20 23:26:29 +0530

    variables and destructuring

---
+++ javascript-definitive-guide-flanagan/chapter3Three/variableDeclarationAndAssignment.js
@@ -0,0 +1,30 @@
+let abc = 1;
+console.log(abc);
+
+abc = "abc";
+console.log(abc);
+
+const xyz = 2;
+console.log(xyz);
+
+console.log(a);
+
+// xyz = 3;
+// console.log(xyz);
+
+var a = 1;
+console.log(a);
+
+var a = 2;
+console.log(a);
+
+let [x, y] = [1, 2];
+console.log(x);
+console.log(y);
+
+let [q, ...p] = [1,2,3,4,5];
+console.log(q);
+console.log(p);
+
+let {a: w, b: e, c: r} = {a:1, b:2, c:3};
+console.log(w,e, r);
