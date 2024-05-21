776b84503505353b5f0428496f90f72361d63a22
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-21 23:00:05 +0530

    regular and conditional expressions

--- javascript-definitive-guide-flanagan/chapter4Four/expressions.js
+++ javascript-definitive-guide-flanagan/chapter4Four/expressions.js
@@ -18,3 +18,22 @@ let func1 = function(x) {
 }
 console.log(func1);
 console.log(func1(2));
+
+console.log(arr[1]);
+let arr2 = [obj, 1, 2, 3, [11, 22]];
+
+console.log(arr2.obj);
+console.log(arr2[0]);
+console.log(arr2["4"]);
+
+console.log(obj?.abc);
+console.log(obj?.abc?.q);
+console.log(arr2?.[8]);
+console.log(arr2[8]);
+console.log(arr2?.[4]);
+
+console.log(func1?.(1));
+console.log(func1?.(1,2));
+console.log(func1?.(5,2, 4, 5, 6));
+// console.log?.(func2?.(2));
+// console.log(func2(2));
