d59b9ccfc870933a54929d0f953d94940d68303d
siddharth2016 <siddharthchandragzb@gmail.com>
2024-05-22 23:14:01 +0530

    unary, binary, ternary and bitwise experssions

--- javascript-definitive-guide-flanagan/chapter4Four/expressions.js
+++ javascript-definitive-guide-flanagan/chapter4Four/expressions.js
@@ -37,3 +37,40 @@ console.log(func1?.(1,2));
 console.log(func1?.(5,2, 4, 5, 6));
 // console.log?.(func2?.(2));
 // console.log(func2(2));
+
+// let tempObj = new obj();
+// console.log(tempObj);
+// let tempObj2 = new obj;
+// console.log(tempObj2)
+console.log("-----------------")
+console.log(-2);
+console.log(2+3);
+console.log(true?1:0);
+
+console.log("3" * 2);
+console.log(1 + "2");
+
+let one = 1;
+console.log(one++);
+console.log(++one);
+
+console.log(2+3*4);
+
+console.log(12 % 5);
+console.log(-12 % 4);
+console.log(-13 % 4);
+console.log(-12 % -5);
+console.log(12 % -5);
+
+console.log(1 + 2 + " blind sum");
+console.log(1 + (2 + " blind sum"));
+
+console.log("-----------------");
+console.log(2 & 3);
+console.log(2 | 3);
+console.log(~2);
+console.log(2 ^ 3);
+console.log(2 >> 3);
+console.log(2 << 3);
+console.log(2 >>> 3);
+console.log(1<<1<<3);
